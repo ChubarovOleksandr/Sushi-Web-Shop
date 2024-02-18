e71e637c5e965e8fa488cba8ab6010e475a12498
Sasha Chubarov <sasha.chubarov.2022@gmail.com>
2024-02-18 10:45:16 +0200

    style sortBy.jsx

--- src/components/shop/SortBy.jsx
+++ src/components/shop/SortBy.jsx
@@ -1,18 +1,36 @@
-/* import React, { useState } from 'react'; */
+import React, { useState, useEffect, useRef } from 'react';
 
 
 export const SortBy = () => {
 
-   /* const [activeIndex, setActiveIndex] = useState(0);
+   const [activeIndex, setActiveIndex] = useState(-1);
+   const [isOpen, setIsOpen] = useState(false);
+   const sortRef = useRef();
 
    const sortByItems = ['За замовчуванням', 'Спочатку дешевше', 'Спочатку дорожче', 'Спочатку легшe', 'Спочатку важчe', 'За акцією'];
-   console.log(activeIndex); */
+
+   useEffect(() => {
+      let toggleIsOpened = event => {
+         if (!event.composedPath().includes(sortRef.current)) {
+            setIsOpen(false);
+         }
+      }
+
+      document.addEventListener('click', toggleIsOpened);
+
+      return () => {
+         document.removeEventListener('click', toggleIsOpened);
+      }
+   }, [isOpen])
 
    return (
-      <div className="shop__sortBy">
-         {/* <ul className="shop__sortBy-list">
-            {sortByItems.map((value, index) => <li onClick={() => setActiveIndex(index)} className='shop__sortBy-item' key={index}>{value}</li>)}
-         </ul> */}
+      <div className="shop__sortBy" ref={sortRef} onClick={()=> setIsOpen(!isOpen)}>
+         <div className="shop__sortBy--active">{activeIndex === -1 ? 'Категорією' : sortByItems[activeIndex]}</div>
+         {isOpen &&
+            <ul className="shop__sortBy-list">
+               {sortByItems.map((value, index) => <li onClick={() => setActiveIndex(index)} className='shop__sortBy-item' key={index}>{value}</li>)}
+            </ul>
+         }
       </div>
    );
 }
