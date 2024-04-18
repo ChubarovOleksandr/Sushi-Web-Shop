33d72c56dbb5edf0f92350649b5c875eb4c9aff9
Sasha Chubarov <sasha.chubarov.2022@gmail.com>
2024-04-18 13:32:30 +0300

    complete accordion on info

--- src/components/info/AccordionItem.jsx
+++ src/components/info/AccordionItem.jsx
@@ -1,31 +1,27 @@
 import { useRef, useState } from "react";
 import '../../scss/components/_Info.scss';
 
-const AccordionItem = ({children}) => {
+const AccordionItem = ({ children, title }) => {
 
-   const [isOpen, setIsOpen] = useState(false);
    const contentRef = useRef();
-   const itemRef = useRef();
 
-   const onClick = () => {
-      setIsOpen(!isOpen);
-      contentRef.current.classList.toggle('active');
-      itemRef.current.classList.toggle('active');
-      if(contentRef.current.style.maxHeight) {
+   const onClick = (e) => {
+      e.target.classList.toggle('active');
+      if (contentRef.current.style.maxHeight) {
          contentRef.current.style.maxHeight = null;
       } else {
-         contentRef.current.style.maxHeight = 200 + 'px';
+         contentRef.current.style.maxHeight = 200 + "px";
       }
    }
 
-   return ( 
-      <div className="accordion__item" ref={itemRef}>
-         <button className="accordion__button" onClick={onClick}>Забери замовлення сам і отримай знижку у 10%!</button>
+   return (
+      <div className="accordion__item">
+         <button className="accordion__button" onClick={onClick}>{title}</button>
          <div className="accordion__content" ref={contentRef}>
-            {isOpen && children}
+            {children}
          </div>
       </div>
-    );
+   );
 }
- 
+
 export default AccordionItem;
